cb0132f774a62b0a19cd2d456f536fba29475dc1
vikash <vikash.t@karza.in>
2022-06-18 22:44:38 +0530

    deleted files which are not required.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,6 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:awesome_notifications_example/pages/phone_call_page.dart';
 import 'package:flutter/material.dart';
-import 'package:awesome_notifications_example/pages/notification_details_page.dart';
 import 'package:awesome_notifications_example/pages/home_page.dart';
 
 const String PAGE_HOME = '/';
@@ -12,9 +11,6 @@ const String PAGE_PHONE_CALL = '/phone-call';
 
 Map<String, WidgetBuilder> materialRoutes = {
   PAGE_HOME: (context) => HomePage(),
-  PAGE_NOTIFICATION_DETAILS: (context) => NotificationDetailsPage(
-        ModalRoute.of(context)!.settings.arguments as ReceivedNotification,
-      ),
-PAGE_PHONE_CALL: (context) =>
+  PAGE_PHONE_CALL: (context) =>
       PhoneCallPage(receivedAction: ModalRoute.of(context)!.settings.arguments as ReceivedAction)
 };
